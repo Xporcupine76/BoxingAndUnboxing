36f2524c53520de884e991e30c325e3141187d8e
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:01:02 +0200

    Add project files.

---
+++ BoxingAndUnboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            object o;//boxing is taking place here
+            int x = 25;
+
+            o = x;
+            Console.WriteLine("o = " + o);
+            Console.WriteLine("x = " + x);
+
+            o = 123;
+            x = (int)o;
+            Console.WriteLine("o = " + o);
+            Console.WriteLine("x = " + x);
+
+
+        }
+    }
+
+
+}
